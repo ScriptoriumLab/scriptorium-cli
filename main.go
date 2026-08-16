be86788baeb331199bed31623da5686dc6a8c948
Edward NoaLand <edwardnoaland@gmail.com>
2026-08-16 11:22:51 +0100

    feat: use cobra to provide greet sub command

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cli = &cobra.Command{
+	Use:   "scriptorium-cli",
+	Short: "A CLI too for enhancing the Scriptorium IME Developer Experience",
+	Long:  "A CLI tool for enhancing the Scriptorium IME Developer Experience",
+}
+
+var greetingCmd = &cobra.Command{
+	Use:   "greet",
+	Short: "Prints a greeting message",
+	Long:  "Prints a greeting message to the console",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, Scriptorium!")
+	},
+}
 
 func main() {
-    fmt.Println("Hello, Scriptorium!")
+	cli.AddCommand(greetingCmd)
+
+	if err := cli.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
